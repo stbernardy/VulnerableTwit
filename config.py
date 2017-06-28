425912ac58a6d9155afc999425b7858b53492253
Cody Bernardy <codybernardyphotos@gmail.com>
2017-06-27 20:53:20 -0700

    Initial Commit
    Config file for bot.py

---
+++ config.py
@@ -0,0 +1,9 @@
+"""
+Get these tokens from apps.twitter.com
+This app only requires Read Only Permissions
+"""
+
+consumer_key = "INSERT CONSUMER KEY HERE"
+consumer_secret = "INSERT CONSUMER SECRET HERE"
+access_key = "INSERT ACCESS KEY HERE"
+access_secret = "INSERT ACCESS SECRET HERE"
